af20507f3e5820fa53953a686979c832f6be1ed9
Timur Kostenko <ktim8168@mail.ru>
2020-06-07 18:40:48 +0300

    resizeable cells

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -6,11 +6,24 @@ import {
   createTable,
 } from './table.template';
 
+import {
+  handleResize,
+} from './table.handlers';
+
 export class Table extends ExcelComponent {
   static className = 'excel__table';
 
   constructor(root) {
-    super(root);
+    super(root, {
+      name: 'Table',
+      listeners: [
+        'mousedown',
+      ],
+    });
+  }
+
+  onMousedown(e) {
+    handleResize(e, this.root);
   }
 
   toHTML() {
